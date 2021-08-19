83a648be014c770ae8de7f9471306ce60ee63a81
ksdevinney <katie@devinney.org>
2021-08-19 14:23:49 -0500

    complete and delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,57 @@
+import { useState, useEffect } from "react";
+
+const API_BASE = "http://localhost:3001";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+
+    console.log(todos);
+  }, []);
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("Error!", err));
+  }
+
+  const completeTodo = async id => {
+    const data = await fetch(API_BASE + ".todo/complete/" + id)
+      .then(res => res.json());
+    setTodos(todos => todos.map(todo => {
+      if (todo._id === data._id) {
+        todo.complete = data.complete;
+      }
+
+      return todo;
+    }));
+  }
+
+  const deleteTodo = async id => {
+    const data = await fetch(API_BASE + ".todo/delete/" + id, { method: "DELETE" })
+      .then(res => res.json());
+
+      setTodos(todos => todos.filter(todo => todo._id !== data._id));
+  }
+
   return (
     <div className="App">
       <h1>Welcome</h1>
       <h4>Your tasks</h4>
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox">
-
-          </div>
-          <div className="text">
-            Get bread
-          </div>
-          <div className="delete-todo">
-            x
-          </div>
+        {todos.map(todo => (
+        <div className={"todo" + (todo.complete ? "is-complete" : "")} key={todo._id} onClick={() => completeTodo(todo._id)}>
+          <div className="checkbox"></div>
+          <div className="text">{ todo.text }</div>
+          <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
         </div>
+        ))}
 
-        <div className="todo is-complete">
-          <div className="checkbox">
-
-          </div>
-          <div className="text">
-            Eat bread
-          </div>
-          <div className="delete-todo">
-            x
-          </div>
-        </div>
       </div>
     </div>
   );
