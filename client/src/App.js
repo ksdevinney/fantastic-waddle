2360fb667ce4fdd02f2ba03ec0b750f172370ac8
ksdevinney <katie@devinney.org>
2021-08-19 14:43:26 -0500

    add new todos to list

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
   }
 
   const completeTodo = async id => {
-    const data = await fetch(API_BASE + ".todo/complete/" + id)
+    const data = await fetch(API_BASE + "/todo/complete/" + id)
       .then(res => res.json());
     setTodos(todos => todos.map(todo => {
       if (todo._id === data._id) {
@@ -33,10 +33,27 @@ function App() {
   }
 
   const deleteTodo = async id => {
-    const data = await fetch(API_BASE + ".todo/delete/" + id, { method: "DELETE" })
+    const data = await fetch(API_BASE + "/todo/delete/" + id, { method: "DELETE" })
       .then(res => res.json());
 
       setTodos(todos => todos.filter(todo => todo._id !== data._id));
+  }
+
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json());
+
+    console.log(data);
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
   }
 
   return (
@@ -53,6 +70,23 @@ function App() {
         ))}
 
       </div>
+
+      <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input 
+            type="text" 
+            className="add-todo-input" 
+            onChange={e => setNewTodo(e.target.value)} 
+            value={newTodo} />
+            <div className="button" onClick={addTodo}>Create Task</div>
+          </div>
+        </div>
+      ) : ""}
     </div>
   );
 }
